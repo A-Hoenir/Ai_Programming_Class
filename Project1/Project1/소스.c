3100238e2da337861013f3aa9c06c8f5bc1069d0
A-Hoenir <jihwan2931@gmail.com>
2021-08-12 16:13:32 +0900

    20210812_Code

---
+++ Project1/Project1/소스.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+
+/*int main(void)
+{
+	int num1 = 15;
+	int num2 = ~num1;
+	printf("not연산의 결과: %d\n", num2);
+	return 0;
+}*/
+
+/*int main()
+{
+	int num = 15;
+	
+	int result1 = num << 1;
+	int result2 = num << 2;
+	int result3 = num << 3;
+
+	printf("1칸 이동: %d\n", result1);
+	printf("2칸 이동: %d\n", result2);
+	printf("3칸 이동: %d\n", result3);
+	return 0;
+}*/
+
+/*int main()
+{
+	char ch = 9;
+	int inum = 1052;
+	double dnum = 3.1415;
+	printf("변수 ch의 크기: %d\n", sizeof(ch));
+	printf("변수 inum의 크기: %d\n", sizeof(inum));
+	printf("변수 dnum의 크기: %d\n", sizeof(dnum));
+
+	printf("char의 크기:%d\n", sizeof(char));
+	printf("int의 크기:%d\n", sizeof(int));
+	printf("long의 크기:%d\n", sizeof(long));
+	printf("long long의 크기:%d\n", sizeof(long));
+	printf("float의 크기:%d\n", sizeof(float));
+	printf("double의 크기:%d\n", sizeof(double));
+	return 0;
+}*/
+
+/*int main()
+{
+	double rad;
+	double area;
+	printf("원의 반지름 입력: ");
+	scanf_s("%lf", &rad);
+
+	area = rad * rad * 3.1415;
+	printf("원의 넓이: %f\n", area);
+	return 0;
+}*/
+
+/*int main()
+{
+	char ch1 = 'A', ch2 = 65;
+	int ch3 = 'Z', ch4 = 90;
+
+	printf("%c %d\n", ch1, ch1);
+	printf("%c %d\n", ch2, ch2);
+	printf("%c %d\n", ch3, ch3);
+	printf("%c %d\n", ch4, ch4);
+	return 0;
+}*/
+
+/*int main()
+{
+	char ch1 = 75, ch2 = 105, ch3 = 109, ch4 = 74, ch5 = 72, ch6 = 119, ch7 = 97, ch8 = 110;
+
+	printf("제 이름은 김지환입니다\n");
+	printf("제 이름은 %c %c %c %c %c %c %c %c %c입니다\n", ch1, ch2, ch3, ch4, ch2, ch5, ch6, ch7, ch8);
+	return 0;
+}*/
+
+/*int main()
+{
+	printf("literal int size: %d\n", sizeof(7));
+	printf("literal double size: %d\n", sizeof(7.14));
+	printf("literal char size: %d\n", sizeof('A'));
+	return 0;
+}*/
+
+/*int main()
+{
+	int num1 = 245;
+	double num2 = 3.1415;
+	int num3 = 129;
+	unsigned char ch = num3;
+
+	printf("정수 245를 실수로: %d\n", num1);
+	printf("실수 3.1415를 정수로: %f\n", num2);
+	printf("큰 정수 129를 작은 정수로: %d\n", ch);
+	return 0;
+}*/
+
+/*int main()
+{
+	int num1 = 3, num2 = 4;
+	double dibResult;
+	dibResult = (double) num1 / num2;
+	printf("나눗셈 결과: %f\n", dibResult);
+	return 0;
+}*/
+
+int main()
+{
+
+}
